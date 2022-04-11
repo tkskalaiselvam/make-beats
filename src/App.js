a5761bb8db463905c78fca4ede3a2c08e8222452
Naresh E <nareshe64@gmail.com>
2022-04-12 00:12:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         
         </p>
-        <h1>hello tks</h1>
+        <h1>hello naresh branch</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
